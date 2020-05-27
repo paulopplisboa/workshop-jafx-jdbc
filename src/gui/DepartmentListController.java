a6e6b098401318ee8f63c716500722efaa950b30
paulopplisboa <silva.pff@gmail.com>
2020-05-27 20:27:21 +0100

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,29 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.swing.plaf.nimbus.State;
+
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +48,10 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsList;
 
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		// System.out.println("onBtNewAction");
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	@Override
@@ -68,6 +81,27 @@ public class DepartmentListController implements Initializable {
 		List<Department> list= service.findAll();
 		obsList=FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			// define que é o stage pai desta janela
+			dialogStage.initOwner(parentStage);
+			// função para travar a janela, enquanto não for fechada não se pode acessar à anterior
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro loading View", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,17 @@
+package gui.util;
+
+import java.awt.event.ActionEvent;
+
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class Utils {
+	
+	// função que devolve o palco actual
+	public static Stage currentStage(javafx.event.ActionEvent event) {
+		
+		return (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+	}
+
+}
