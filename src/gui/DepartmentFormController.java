8a2ffec679ff90e182b3076899aecb1b4ae3f2ed
paulopplisboa <silva.pff@gmail.com>
2020-05-28 10:56:29 +0100

    Save a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,16 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 // classe para controlar a parte gráfica do departamento 
 public class DepartmentFormController implements Initializable {
+	
+	private DepartmentService service;
 	
 	// entidade relacionada com este formulário
 	private Department entity;
@@ -37,13 +45,43 @@ public class DepartmentFormController implements Initializable {
 		this.entity=entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service=service;
 	}
 	
 	@FXML
-	public void onBtCancelAction() {
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity==null) {
+			throw new IllegalStateException("entity was null");	
+			}
+		if (service==null) {
+			throw new IllegalStateException("service was null");	
+			}
+		try {
+		//System.out.println("onBtSaveAction");
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		// pega a referencia da janela actual e em seguida fecha a janela
+		Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving obj", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+		
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 		System.out.println("onBtCancelAction");
 	}
 	
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -20,7 +20,15 @@ public class DepartmentService {
 		// return list;
 
 		return dao.findAll();
-
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId()==null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
 	}
 
 }
