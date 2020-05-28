307a4c832ee75a1463a2315d44b3b12b3cfba025
paulopplisboa <silva.pff@gmail.com>
2020-05-28 22:23:33 +0100

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.Map;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 // classe para controlar a parte gráfica do departamento 
@@ -77,6 +80,9 @@ public class DepartmentFormController implements Initializable {
 		// pega a referencia da janela actual e em seguida fecha a janela
 		Utils.currentStage(event).close();
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
 		catch (DbException e) {
 			Alerts.showAlert("Error saving obj", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -93,8 +99,16 @@ public class DepartmentFormController implements Initializable {
 
 	private Department getFormData() {
 		Department obj = new Department();
+		ValidationException exception = new ValidationException("validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if (txtName.getText() == null || txtName.getText().trim().equals("") ) {
+			exception.addError("name", "field can´t be empty");
+		}
 		obj.setName(txtName.getText());
+		if (exception.getErros().size()>0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -129,6 +143,14 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());		
+	}
+	
+	private void setErrorMessages (Map<String, String> errors) {
+		Set<String> fields=  errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErroName.setText(errors.get("name"));
+		}
 	}
 
 }
