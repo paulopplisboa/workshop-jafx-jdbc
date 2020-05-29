2e738ca00c729a7263aaa65101908de949325609
paulopplisboa <silva.pff@gmail.com>
2020-05-29 12:32:17 +0100

    Seller TableView

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,25 +1,66 @@
 package gui.util;
 
-import java.awt.event.ActionEvent;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import javafx.stage.Stage;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 
 public class Utils {
-	
+
 	// função que devolve o palco actual
 	public static Stage currentStage(javafx.event.ActionEvent event) {
-		
-		return (Stage)((Node)event.getSource()).getScene().getWindow();
-		
+
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+
 	}
 
 	public static Integer tryParseToInt(String str) {
 		try {
-		return Integer.parseInt(str);
-		}
-		catch (NumberFormatException e) {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
 			return null;
 		}
 	}
+
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
 }
